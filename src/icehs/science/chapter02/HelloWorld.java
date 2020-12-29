7946c03cfec1244d3a144ea4eba56d024e936e44
User <User@DESKTOP-C151SN4>
2020-12-29 20:32:36 +0900

    chapter03

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,6 +1,15 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
+	//동인천고등학교 
+	//집중이수제
+	//응용프로그래밍 개발
+	//정재환
+	/*
+	 * 동인천고등학교
+	 * 집중이수제
+	 * 응용프로그래밍 개발
+	 * 정재환*/
 
 	public static void main(String[] args) {
 	System.out.println("Hello World!!!");
