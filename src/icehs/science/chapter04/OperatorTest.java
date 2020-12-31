6945cdaeef8f36f7919bead15e2a11ec401b021d
User <User@DESKTOP-C151SN4>
2020-12-31 20:47:50 +0900

    chapter05

--- src/icehs/science/chapter04/OperatorTest.java
+++ src/icehs/science/chapter04/OperatorTest.java
@@ -10,10 +10,10 @@ public class OperatorTest {
 		boolean result3 = (num % 2 == 0) && (num % 7 == 0);
 		boolean result4 = (num % 2 == 0) || (num % 7 == 0);
 		
-		System.out.println("짝수입니까? : " + (num % 2 == 0));
-		System.out.println("3의 배수입니까? : " + (num % 3 == 0));
-		System.out.println("2의 배수이면서 7의 배수입니까? : " + (num % 2 == 0) && (num % 7 == 0));
-		System.out.println("2의 배수이거나 7의 배수입니까? : " + (num % 2 == 0) || (num % 7 == 0));
+		System.out.println("짝수입니까? : " + result1);
+		System.out.println("3의 배수입니까? : " + result2);
+		System.out.println("2의 배수이면서 7의 배수입니까? : " + result3);
+		System.out.println("2의 배수이거나 7의 배수입니까? : " + result4);
 	}
 
 }
